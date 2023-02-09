149c33795ed9e6ee581e4231e1aac053f60484cb
Danial-Ismail <121781837+Danial-Ismail@users.noreply.github.com>
2023-02-09 21:26:23 +0500

    added index and js files

---
+++ Assignment-4/chap 12-13/app.js
@@ -0,0 +1,89 @@
+//***** Chapter 12-13 ****////
+
+//Question 1
+
+// var ch=prompt("Enter a character")
+// if(ch>=65 && ch<=90){
+//     alert("ch is an uppercase letter")
+// }
+// else if(ch>=97 && ch<=122){
+//     alert("ch is an lowercase letter")
+// }
+// else{
+//     alert("Number")
+// }
+
+//Question 2
+
+// var a=5
+// var b=7
+// if(a>b){
+//     alert("a is greater than b")
+// }
+// else if(a<b){
+//     alert("a is less than b")
+// }
+// else{
+//     alert("a is equal to b")
+// }
+
+//Question 3
+
+// var num=+prompt("Enter a num")
+// if(num>0){
+//     alert("The num is positive")
+// }
+// else if(num<0){
+//     alert("The number is negative")
+// }
+// else{
+//     alert("The number is zero")
+// }
+
+//Question 4
+
+// var ch=prompt("Enter a character")
+// if(ch==="a" || ch==="e" || ch==="i" || ch==="o" || ch==="u"){
+//     alert("True")
+// }
+// else{
+//     alert("false")
+// }
+
+
+//Question 5
+
+// var originalPassword="I love you"
+// var password=prompt("Enter your password")
+// if(password===originalPassword){
+//     alert("correct")
+// }
+// else{
+//     alert("Incorrect password")
+// }
+
+//Question 6
+
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";}
+// else{
+// greeting = "Good evening";
+// }
+
+//Question 7
+// var time=1900
+// var time=prompt("Enter a time")
+// if(time>=0000 && time<1200){
+//     alert("Good Morning")
+// }
+// else if(time>=1200 && time<1700){
+//     alert("Good afternoon")
+// }
+// else if(time>=1700 && time<2100){
+//     alert("Good evening")
+// }
+// else{
+//     alert("Good night")
+// }
---
+++ Assignment-4/chap 9-11/app.js
@@ -0,0 +1,222 @@
+//****** Chapter 9-11 ******///
+
+//Question1
+
+//var city=prompt("Enter city name")
+//if (city==="karachi"){
+  //  alert("Welcome to the city of lights")
+//}
+
+//Question 2
+
+// //var gender=prompt("Enter your gender")
+// if (gender==="male"){
+//     alert("Good Morning Sir.")
+// }
+// if(gender==="female")
+// alert("Good Morning Ma'am.")
+
+//Question 3
+
+ //var color=prompt("Enter color")
+ //if(color==="red"){
+     //alert("must stop")
+ //}
+ //else if (color==="yellow"){
+    //alert("ready to move")
+ //}
+ //else if(color==="green"){
+     //alert("move now")
+ //}
+
+//Question 4
+
+//var remainingFuel=prompt("Enter remaining fuel in ltrs")
+//if (remainingFuel<"0.25"){
+    //alert("Please refill the fuel in your car")
+//}
+
+//Question 5
+//a
+//var a =4;
+//if (++a===5){
+    //alert("given condition for variable is true")
+//}
+
+//b
+//var b=82;
+//if(b++===83){
+    //alert("given condition for variable b is true");
+//}
+//Not displayed
+
+//c
+//var c=12;
+//if(c++===13){
+    //alert("condition 1 is true")
+//}
+//Not displayed
+
+//if(c===13){
+    //alert("condition 2 is true")
+//}
+//displayed
+
+//if(++c<14){
+    //alert("condition 3 is true")
+//}
+//not displayed
+
+//if(c===14){
+    //alert("condition 4 is true")
+//}
+ //not displayed
+
+ //d
+
+// var materialCost=20000;
+// var laborCost=2000
+// var totalCost=materialCost+laborCost;
+// if("totalcost===laborCost+materialCost"){
+//     alert("the cost equals")
+// }
+
+//e
+
+//if(true){
+    //alert("true");
+//}
+//if(false){
+    //alert("false");
+//}
+
+//f
+
+//if("car"<"cat"){
+    //alert("car is samller than cat")
+//}
+
+//Question 6
+
+// var phyObtainedMarks=prompt("Enter phymarks")
+// var totalMarks=300
+// var percentage=phyObtainedMarks/totalMarks*100
+// if(percentage>=80){
+//     alert("A-one grade")
+//     alert("Excellent")
+// }
+// else if(percentage>=70){
+//     alert("A Grade")
+//     alert("Good")
+// }
+// else if(percentage>=60){
+//     alert("B Grade")
+//     alert("You need to improve")
+// }
+// else{
+//     alert("fail")
+// }
+// var chemObtainedMarks=prompt("Enter chemmarks")
+// var percentage=chemObtainedMarks/totalMarks*100
+// if(percentage>=80){
+//     alert("A-one grade")
+//     alert("Excellent")
+// }
+// else if(percentage>=70){
+//     alert("A Grade")
+//     alert("Good")
+// }
+// else if(percentage>=60){
+//     alert("B Grade")
+//     alert("You need to improve")
+// }
+// else{
+//     alert("fail")
+// }
+// var mathObtainedMarks=prompt("Enter mathmarks")
+// var percentage=mathObtainedMarks/totalMarks*100
+// if(percentage>=80){
+//     alert("A-one grade")
+//     alert("Excellent")
+// }
+// else if(percentage>=70){
+//     alert("A Grade")
+//     alert("Good")
+// }
+// else if(percentage>=60){
+//     alert("B Grade")
+//     alert("You need to improve")
+// }
+// else{
+//     alert("fail")
+// }
+
+//Question 7
+
+//var secretNumber=prompt("Enter your lucky number")
+// if(secretNumber==="5"){
+//     alert("Bingo! Correct answer")
+// }
+// else if(secretNumber==="6"){
+//     alert("Close enough to the correct answer.")
+// }
+// else{
+//     alert("Better luck next time")
+// }
+
+//Question 8
+
+//var number=24
+//if(number%3==0){
+    //alert("the number is divisible by 3")
+//}
+    
+//Question 9
+
+// var num=prompt("Enter a number")
+// if (num==="2"){
+//     alert("The given number is an odd number")
+// }
+// else if (num==="3"){
+//     alert("The given number is an even number")
+// }
+
+//Question 10
+
+// var T=prompt("Enter a temperature")
+// if(T>40){
+//     alert("It's too hot outside.")
+// }
+// else if(T>30){
+//     alert("The weather today is normal")
+// }
+
+// else if(T>20){
+//     alert("Today's weather is cool")
+// }
+
+// else {
+//     alert("OMG! Today's weather is so cool")
+// }
+
+//Question 11
+
+// var num1=+prompt("Enter num1");
+// var opt=prompt("Enter opt");
+// var num2=+prompt("Enter num2");
+
+// if(opt=="+"){
+//     alert(num1+num2)
+// }
+//  if(opt=="-"){
+//     alert(num1-num2)
+// }
+//  if(opt=="*"){
+//     alert(num1*num2)
+// }
+//  if(opt=="/"){
+//     alert(num1/num2)
+// }
+//  if(opt=="%"){
+//     alert(num1%num2)
+// }
